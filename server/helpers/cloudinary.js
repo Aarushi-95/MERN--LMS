252b37787db72cd4b672cb206212879a59cede36
Aarushi Agarwal <agarwalaarushi95@gmail.com>
2024-11-02 23:00:12 +0530

    Done

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2
 
 //configure with env data
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: "dazlvvjqu",
+    api_key: "842992631299949",
+    api_secret: "3qTETgiZZRVIlEaE49bxXX26zeE",
 });
 
 const uploadMediaToCloudinary = async(filePath)=> {
